9eaaa01a2d7948228db567c001cc1afb71ddce76
Francisco Martins <chicomm05@gmail.com>
2025-03-14 05:41:25 -0300

    Initial commit

---
+++ vm.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+#include <Windows.h>
+#include <conio.h>  
+
+enum { R_R0, R_R1, R_R2, R_R3, R_R4, R_R5, R_R6, R_R7, R_PC, R_COND, R_COUNT };
+enum { FL_POS = 1 << 0, FL_ZRO = 1 << 1, FL_NEG = 1 << 2 };
+enum { OP_BR, OP_ADD, OP_LD, OP_ST, OP_JSR, OP_AND, OP_LDR, OP_STR, OP_RTI, OP_NOT, OP_LDI, OP_STI, OP_JMP, OP_RES, OP_LEA, OP_TRAP };
+enum { MR_KBSR = 0xFE00, MR_KBDR = 0xFE02 };
+enum { TRAP_GETC = 0x20, TRAP_OUT, TRAP_PUTS, TRAP_IN, TRAP_PUTSP, TRAP_HALT };
+
+#define MEMORY_MAX (1 << 16)
+uint16_t memory[MEMORY_MAX];
+uint16_t reg[R_COUNT];
+HANDLE hStdin = INVALID_HANDLE_VALUE;
+DWORD fdwMode, fdwOldMode;
+
+void disable_input_buffering() {
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hStdin, &fdwOldMode);
+    fdwMode = fdwOldMode ^ ENABLE_ECHO_INPUT ^ ENABLE_LINE_INPUT;
+    SetConsoleMode(hStdin, fdwMode);
+    FlushConsoleInputBuffer(hStdin);
+}
+
+void restore_input_buffering() {
+    SetConsoleMode(hStdin, fdwOldMode);
+}
+
+uint16_t check_key() {
+    return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit();
+}
+
+void handle_interrupt(int signal) {
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
+uint16_t sign_extend(uint16_t x, int bit_count) {
+    if ((x >> (bit_count - 1)) & 1) {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
+uint16_t swap16(uint16_t x) {
+    return (x << 8) | (x >> 8);
+}
+
+void update_flags(uint16_t r) {
+    if (reg[r] == 0) reg[R_COND] = FL_ZRO;
+    else if (reg[r] >> 15) reg[R_COND] = FL_NEG;
+    else reg[R_COND] = FL_POS;
+}
+
+void read_image_file(FILE* file) {
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), MEMORY_MAX - origin, file);
+    while (read-- > 0) { *p = swap16(*p); ++p; }
+}
+
+int read_image(const char* image_path) {
+    FILE* file = fopen(image_path, "rb");
+    if (!file) return 0;
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
+void mem_write(uint16_t address, uint16_t val) {
+    memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address) {
+    if (address == MR_KBSR) {
+        if (check_key()) {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        } else {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
+
+int main(int argc, const char* argv[]) {
+    if (argc < 2) {
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+    for (int j = 1; j < argc; ++j) {
+        if (!read_image(argv[j])) {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
+    reg[R_COND] = FL_ZRO;
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+    int running = 1;
+    while (running) {
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+        switch (op) {
+            case OP_ADD: {
+                uint16_t r0 = (instr >> 9) & 0x7, r1 = (instr >> 6) & 0x7;
+                uint16_t imm_flag = (instr >> 5) & 0x1;
+                reg[r0] = imm_flag ? reg[r1] + sign_extend(instr & 0x1F, 5) : reg[r1] + reg[instr & 0x7];
+                update_flags(r0);
+            } break;
+            case OP_AND: {
+                uint16_t r0 = (instr >> 9) & 0x7, r1 = (instr >> 6) & 0x7;
+                uint16_t imm_flag = (instr >> 5) & 0x1;
+                reg[r0] = imm_flag ? reg[r1] & sign_extend(instr & 0x1F, 5) : reg[r1] & reg[instr & 0x7];
+                update_flags(r0);
+            } break;
+            case OP_NOT: {
+                uint16_t r0 = (instr >> 9) & 0x7, r1 = (instr >> 6) & 0x7;
+                reg[r0] = ~reg[r1];
+                update_flags(r0);
+            } break;
+            case OP_BR: {
+                uint16_t pc_offset = sign_extend(instr & 0x1FF, 9), cond_flag = (instr >> 9) & 0x7;
+                if (cond_flag & reg[R_COND]) reg[R_PC] += pc_offset;
+            } break;
+            case OP_JMP: reg[R_PC] = reg[(instr >> 6) & 0x7]; break;
+            case OP_JSR: {
+                reg[R_R7] = reg[R_PC];
+                uint16_t long_flag = (instr >> 11) & 1;
+                reg[R_PC] = long_flag ? reg[R_PC] + sign_extend(instr & 0x7FF, 11) : reg[(instr >> 6) & 0x7];
+            } break;
+            case OP_LD: {
+                uint16_t r0 = (instr >> 9) & 0x7;
+                reg[r0] = mem_read(reg[R_PC] + sign_extend(instr & 0x1FF, 9));
+                update_flags(r0);
+            } break;
+            case OP_TRAP:
+                reg[R_R7] = reg[R_PC];
+                if ((instr & 0xFF) == TRAP_HALT) running = 0;
+                break;
+            default: abort();
+        }
+    }
+    restore_input_buffering();
+}
